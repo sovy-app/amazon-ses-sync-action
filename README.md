d2140f0d8cecd1bff33b478d10e5bef287ac97ca
Rafal Swietek <raf.swietek@gmail.com>
2022-02-22 13:52:05 +0000

    First test full version

---
+++ README.md
@@ -0,0 +1,57 @@
+# Sovy - Amazon SES Synch Action
+
+This action synchronizes email templates from within a Github repo with Amazon SES.
+
+## Inputs
+
+### `templates_dir`
+
+**Required** A path to a directory where your email templates are stored, e.g. `${{ github.workspace }}/templates/`.
+
+### `token`
+
+**Required** A Sovy Github Synch Token generated inside the https://sovy.app dashboard.
+
+## How to prepare your repo
+
+### Repository file structure
+Keep all your email templates in separate files, in a single directory, e.g. `/templates`.
+
+### Defining email subject
+Each template file should contain a single email subject defined like this:
+```html
+<!-- subject: Here put your email subject -->
+```
+
+## How it works
+
+1. Listing all files from the provided directory `templates_dir`.
+2. Reading contents of all found files.
+3. Parsing files' contents looking for the HTML comment containing a **subject**.
+4. Synching all templates with Amazon SES through Sovy using provided `token`.
+
+If a template with a specific name does not exist in your Amazon SES account, a new template will be created.
+
+If a template with a specific name already exists, it will be updated.
+
+If a template that was synched through this Github Action before (created or updated) is now missing, it will be deleted from the Amazon SES as well.
+
+If you already have templates in your Amazon SES account and there are no templates with corresponding names inside your Github repo, those templates will stay untouched.
+
+## Example usage
+
+```yaml
+steps:
+  - name: Checking out the repo
+    uses: actions/checkout@v2
+  - name: Setting up node
+    uses: actions/setup-node@v2
+    with:
+      node-version: '16'
+      cache: 'npm'
+  - name: Synching email templates
+    uses: sovy/amazon-ses-synch-action@v1
+    with:
+      templates_dir: ${{ github.workspace }}/templates/
+      token: 60e77e49-8625-4b97-81ac-d573b6edbd36
+```
---
+++ index.js
@@ -0,0 +1,79 @@
+const core = require('@actions/core');
+const styles = require('ansi-styles');
+const fs = require('fs');
+
+function getNameFromFileName(fileName) {
+  const fileNameParts = fileName.split('.');
+  if (fileNameParts.length === 1) return fileName;
+  fileNameParts.pop();
+  return fileNameParts.join('.');
+}
+
+function getSubjectFromFileContent(content) {
+  const regexp = /<!-- subject:(.*)-->/gmi
+  const match = content.match(regexp);
+  return match.trim();
+}
+
+async function main() {
+  try {
+    const templatesDir = core.getInput('templates_dir');
+    const token = core.getInput('token');
+
+    if (!templatesDir) {
+      core.setFailed('Input parameter templates_dir is missing.');
+      return;
+    }
+
+    if (!token) {
+      core.setFailed('Input parameter token is missing.');
+      return;
+    }
+
+    core.info(`Templates directory: ${templatesDir}`);
+    core.info('.');
+
+    const templateFileNames = fs.readdirSync(templatesDir);
+    core.info(`Found ${templateFileNames.length} files in the provided directory:`);
+    templateFileNames.forEach((fileName, index) => core.info(`${index + 1}. ${fileName}`));
+    core.info('.');
+
+    core.info('Reading files...');
+    const templateFiles = templateFileNames.map((fileName) => ({
+      name: fileName,
+      content: fs.readFileSync(`${templatesDir}${fileName}`),
+    }));
+    core.info(`All ${templateFiles.length} files read successfully.`);
+    core.info('.');
+
+    core.info('Parsing files...');
+    const emailTemplates = templateFiles.map((templateFile, index) => {
+      core.info(`Parsing file ${index + 1}. ${templateFile.name}`);
+      return {
+        name: getNameFromFileName(templateFile.name),
+        subject: getSubjectFromFileContent(templateFile.content),
+        html: templateFile.content,
+      };
+    });
+    core.info(`All ${emailTemplates.length} files parsed successfully.`);
+
+    core.info('Synching email templates...');
+    await Promise.all(emailTemplates.map(template => {
+      core.info(`Synching ${template.name}`);
+      return fetch(`https://sovy.app/api/synch/${token}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(template),
+      });
+    }));
+    core.info(`All ${emailTemplates.length} templates synched successfully.`);
+    core.info('.');
+    core.info(`${styles.green.open}All good, we're done here!${styles.green.close}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
